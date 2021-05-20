80ae059b0bed5e93c09e379872eba2fffbc207aa
Yi, Damyeong | 이담영 <yidamyeong@gmail.com>
2021-05-20 21:49:05 +0900

    modify ascii -> encoding

--- gigone/encoding.py
+++ gigone/encoding.py
@@ -7,6 +7,14 @@ def print_text(text, encoding_type):
     print('\'' + text + '\' 전체 문자를 표현하는 데 사용한 바이트 수: {0} 바이트'.format(len(byte_data)))
     print('\'' + text + '\' 16진수 값: {0}'.format(hex_data_as_str))
     print('\'' + text + '\' 10진수 값: {0}'.format(int_data_as_str))
+    print('--------------------------------------------------------')
 
 
 print_text('Hello', 'ascii')
+print_text('Hello', 'euc-kr')
+print_text('안녕하세요', 'euc-kr')
+print_text('Hello', 'utf-8')
+print_text('안녕하세요', 'utf-8')
+print_text('Hello', 'utf-16')
+print_text('안녕하세요', 'utf-16')
+
