ff363725a5479bdd967a17ff40f66650cb9dd8ed
Yi, Damyeong | 이담영 <yidamyeong@gmail.com>
2021-05-25 22:26:56 +0900

    add random_uuid

---
+++ gigone/random_uuid.py
@@ -0,0 +1,9 @@
+import uuid
+
+for i in range(1, 5):
+    print('랜덤 UUID({0})={1}'.format(i, uuid.uuid4()))
+
+# 랜덤 UUID(1)=caa487b0-3ee4-4e1c-86eb-fc449a6fd1f5
+# 랜덤 UUID(2)=1aa3650b-ae5d-420d-9114-afda66ffd875
+# 랜덤 UUID(3)=15d73bdf-adc7-4a0f-8b13-4fe4383c5e23
+# 랜덤 UUID(4)=84eebf68-a4ca-4c84-ac0e-275eb3d050a7
