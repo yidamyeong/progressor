c1f72e49bb492b3fbcd7cb68b3a18329b9b86736
Yi, Damyeong | 이담영 <yidamyeong@gmail.com>
2021-06-01 22:28:00 +0900

    add comp_then_decomp

--- gigone/comp_then_decomp.py
+++ gigone/comp_then_decomp.py
@@ -10,16 +10,21 @@ def open_json_file(filename):
             print('JSON 데이터를 파싱하는 데 실패했습니다. 사유={0}'.format(e))
             return None
 
+
 json_object = open_json_file('message1.json')
 json_str = json.dumps(json_object, ensure_ascii=False)
 json_byte_data = json_str.encode('utf8')
 
-# level 은 1-9까지 지정 가능. 1은 가장 빠르나 압축률이 낮다.
-# 9는 가장 느리나 압축률이 높다. 기본값 -1은 6과 동일하다.
+# level 은 1-9까지 지정 가능하다. 1은 가장 빠르고 압축률이 낮고 9는 가장 느리지만 압축률이 높다. 기본값 -1은 6과 동일하다.
 compressed_data = zlib.compress(json_byte_data, level=-1)
+
 # 압축된 데이터의 CRC32 체크섬을 계산한다.
 crc32 = zlib.crc32(compressed_data)
 
 print('\'json_str\' 데이터 길이={0}'.format(len(json_byte_data)))
 print('압축된 \'compressed_json\' 데이터 길이={0}'.format(len(compressed_data)))
 print('압축된 \'compressed_json\' 데이터 CRC2={0}'.format(crc32))
+
+decompressed_data = zlib.decompress(compressed_data)
+print('압축 해제된 \'decompressed_json\' 길이={0}'.format(len(decompressed_data)))
+print('decompressed_data(UTF8)={0}'.format(decompressed_data.decode('utf8')))
